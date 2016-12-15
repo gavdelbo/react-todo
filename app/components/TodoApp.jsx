4023743dbf56ce9ac0871d5fe19d0bdd35486fc5
Gavin D <gavdelbo@gmail.com>
2016-12-16 07:14:40 +1100

    Initial TodoApp.jsx setup.

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+const TodoApp = React.createClass({
+
+  render: function () {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    );
+  }
+
+});
+
+module.exports = TodoApp;
